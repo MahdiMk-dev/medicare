3a316d007713868f7133b55134c4c9b8deab0443
Mahdi Mokalled <mokaledmahdi@gmail.com>
2024-05-04 17:19:24 +0300

    Login/signup page static design

---
+++ user-side/frontend/user-frontend/src/pages/Auth.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import AuthComponent from '../components/AuthComponent';
+
+function Auth() {
+  return(
+  <AuthComponent/>
+  )
+}
+
+export default Auth;
