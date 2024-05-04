f84a15b54201b507ee255b559383ee2227e8a517
Mahdi Mokalled <mokaledmahdi@gmail.com>
2024-05-04 20:43:28 +0300

    Request nurse form page

---
+++ user-side/frontend/user-frontend/src/pages/RequestNurse.js
@@ -0,0 +1,13 @@
+import React from "react";
+import RequestNurseComponent from "../components/RequestNurseComponent";
+import Navbar from "../components/navbar";
+
+function RequestNurse(){
+    return(
+        <div>
+        <Navbar/>
+        <RequestNurseComponent/>
+        </div>
+    )
+}
+export default RequestNurse;
