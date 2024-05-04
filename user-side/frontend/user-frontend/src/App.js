b8a1f4865927a64496d1a3709a58d46418b50f6b
Mahdi Mokalled <mokaledmahdi@gmail.com>
2024-05-05 00:37:25 +0300

    updated routing

--- user-side/frontend/user-frontend/src/App.js
+++ user-side/frontend/user-frontend/src/App.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Auth from './pages/auth';
+import Auth from './pages/Auth';
 import Home from './pages/Home';
-
+import RequestNurse from './pages/RequestNurse';
+import RequestDoctor from './pages/RequestDoctor';
+import RequestPhlebotomist from './pages/RequestPhlebotomist';
+import Profile from './pages/Profile';
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/auth" element={<Auth />} />
         <Route path="/home" element={<Home />} />
+        <Route path="/requestNurse" element={<RequestNurse/>}/>
+        <Route path="/requestDoctor" element={<RequestDoctor/>}/>
+        <Route path="/requestPhlebotomist" element={<RequestPhlebotomist/>}/>
+        <Route path="/profile" element ={<Profile/>}/>
 
       </Routes>
 
