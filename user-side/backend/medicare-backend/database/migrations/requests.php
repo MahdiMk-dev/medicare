0eaca4a78942a928e692c55e61876be78eeddd7b
Mahdi Mokalled <mokaledmahdi@gmail.com>
2024-05-06 19:43:03 +0300

    requests migration

---
+++ user-side/backend/medicare-backend/database/migrations/requests.php
@@ -0,0 +1,27 @@
+<?php
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRequestsTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('requests', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id');
+            $table->foreignId('service_id');
+            $table->dateTime('start')->nullable();
+            $table->dateTime('end')->nullable();
+            $table->string('status')->default('pending');
+            $table->tinyInteger('urgent')->nullable();
+            $table->text('image')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('requests');
+    }
+}
