facbf8c18dcbe651cc6aefaa2e7938bdf78c79f5
Mahdi Mokalled <mokaledmahdi@gmail.com>
2024-05-09 13:58:46 +0300

    routes in api

---
+++ user-side/backend/medicare-backend/routes/api.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DutyController;
+use App\Http\Controllers\UserController;
+
+
+
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::get('/hello', [AuthController::class, 'hello']);
+    Route::group(['middleware' => 'auth:api'], function () {
+    Route::post('logout', [AuthController::class, 'logout']);
+    Route::get('me', [AuthController::class, 'me']);
+    Route::post('/users', [UserController::class, 'store']);
+    Route::get('/show', [UserController::class, 'show']); 
+    Route::put('/users/{id}', [UserController::class, 'update']);    
+    Route::post('/duties', [DutyController::class, 'store']);
+    Route::put('/duties/{id}', [DutyController::class, 'update']);
+    Route::delete('/duties/{id}', [DutyController::class, 'destroy']);
+});
