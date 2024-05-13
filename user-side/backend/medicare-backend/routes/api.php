b77fccf0ee4c8315a46a4685abfedd4dcefc3dec
Mahdi Mokalled <mokaledmahdi@gmail.com>
2024-05-14 00:20:15 +0300

    updated routes

--- user-side/backend/medicare-backend/routes/api.php
+++ user-side/backend/medicare-backend/routes/api.php
@@ -1,13 +1,14 @@
 <?php
 
-use App\Http\Controllers\MedicationController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\DutyController;
 use App\Http\Controllers\RequestController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\MedicationController;
 use Symfony\Component\Routing\RequestContext;
+
 
     Route::post('/login', [AuthController::class, 'login']);
     Route::post('/register', [AuthController::class, 'register']);
@@ -19,11 +20,10 @@ use Symfony\Component\Routing\RequestContext;
     Route::post('/duties', [DutyController::class, 'store']);
     Route::put('/duties/{id}', [DutyController::class, 'update']);
     Route::delete('/duties/{id}', [DutyController::class, 'destroy']);
-    Route::post('/createRequest', [RequestController::class,'createRequest']);
-    Route::post('/editRequest',[RequestController::class,'editRequest'] );
-    Route::post('/createMedication', [MedicationController::class , 'addMedication']);
-    Route::post('/editMedication', [MedicationController::class , 'editMedication']);
-    Route::post('/deleteMedication', [MedicationController::class , 'deleteMedication']);
-
-
-;
+    Route::post('/create-request', [RequestController::class,'create_request']);
+    Route::post('/edit_profile', [UserController::class,'editinfo']);
+    Route::delete('/delete_medication/{id}', [MedicationController::class,'deleteMedication']);
+    Route::post('/add_medication', [MedicationController::class,'addMedication']);
+    Route::post('/medication/{id}', [MedicationController::class,'editMedication']);
+    Route::get('/get_medication/{id}', [MedicationController::class,'getmedication']);
+    
