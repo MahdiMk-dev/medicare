5114a46eba91de46f5cae81438016e68c222cd0e
Mahdi Mokalled <mokaledmahdi@gmail.com>
2024-05-13 14:37:30 +0300

    order model

---
+++ user-side/backend/medicare-backend/app/Models/Order.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Order extends Model
+{
+    use HasFactory;
+    protected $table = 'requests';
+    public function service(): BelongsTo
+    {
+        return $this->belongsTo(Service::class);
+    }
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
