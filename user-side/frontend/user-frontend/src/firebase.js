d8104664190b0fbee3064d11c5dfbdda3d5b77eb
Mahdi Mokalled <mokaledmahdi@gmail.com>
2024-05-13 14:39:40 +0300

    firebase file

---
+++ user-side/frontend/user-frontend/src/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAVFaFhfJDX6xB8wy6hOpy8w1OgvQymFoY",
+  authDomain: "medicare-55710.firebaseapp.com",
+  projectId: "medicare-55710",
+  storageBucket: "medicare-55710.appspot.com",
+  messagingSenderId: "470777842935",
+  appId: "1:470777842935:web:08d179df925f47f25b17f2",
+  measurementId: "G-BE8P63JTC3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
