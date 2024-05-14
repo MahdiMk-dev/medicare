c7801871c8d2865548f023e4382841f31cea248d
Mahdi Mokalled <mokaledmahdi@gmail.com>
2024-05-14 15:58:28 +0300

    basic routes admin

--- admin/medicare-admin/src/App.js
+++ admin/medicare-admin/src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React from "react";
+
+import Home from "./components/Home";
+import UserList from "./components/UserList";
+import User from "./components/User";
+import NewUser from "./components/NewUser";
+import AdminLogin from "./components/AdminLogin";
+import AdminChat from "./components/AdminChat";
+import DisplayChats from "./components/DisplayChats";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+          <Route path="/admin" element={<Home />} />
+          <Route path="/users" element={<UserList />} />
+          <Route path="/user/:userId" element={<User />} />
+          <Route path="/admin_login" element={<AdminLogin />} />
+          <Route path="/newUser" element={<NewUser />} />
+          <Route path="/displaychats" element={<DisplayChats />} />
+          <Route path="/newchat" element={<AdminChat />} />
+          <Route path="/chat/:patientId" element={<AdminChat />} />
+  
+        </Routes>
+    </Router>
   );
 }
 
