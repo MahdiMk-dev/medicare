a3d753eed687d3f7afa10d395fea4134705a8031
Mahdi Mokalled <mokaledmahdi@gmail.com>
2024-05-14 22:29:52 +0300

    handle image url, result analysis

--- user-side/backend/testanalysis/app.py
+++ user-side/backend/testanalysis/app.py
@@ -1,15 +1,21 @@
-from flask import Flask, request
+from flask import Flask, flash, request, redirect, url_for
 import pytesseract
 import cv2
 import numpy as np
 from openai import OpenAI
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+from werkzeug.utils import secure_filename
+import os
 
+
+UPLOAD_FOLDER = './images'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:@localhost/medicare'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 db = SQLAlchemy(app)
 CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})
 
@@ -24,10 +30,32 @@ class ResultAnalysis(db.Model):
         print(self.user_id)
         return f'<ResultAnalysis id={self.id}, image_url={self.image_url}, result={self.result},user_id={self.user_id}>'
 
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'image' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['image']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file',
+                                    filename=filename))
+
 @app.route('/', methods=['POST'])
 def hello_world():
     # Save the image URL
-    image_url = request.form.get('image_url')
+    image_url = upload_file()
     user_id = request.form.get('user_id')
     print(user_id)
     # Your existing code for OCR
