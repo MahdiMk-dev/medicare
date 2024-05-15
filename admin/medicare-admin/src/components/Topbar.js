6ea19fe9e1d909f9077a322fbab43b094e0aba32
Mahdi Mokalled <mokaledmahdi@gmail.com>
2024-05-15 12:40:57 +0300

    adjusted top bar-admin

--- admin/medicare-admin/src/components/Topbar.js
+++ admin/medicare-admin/src/components/Topbar.js
@@ -1,9 +1,12 @@
-import React from "react";
+import React, {useState} from "react";
 import '../styles/topbar.css'
 import adminImage from '../images/icon-admin-32.png';
 // import "./topbar.css";
 import { NotificationsNone, Language, Settings } from "@mui/icons-material";
 function Topbar() {
+  const aimage=localStorage.getItem('image_url');
+
+
   return (
     <div className="topbar">
       <div className="topbarWrapper">
@@ -12,7 +15,10 @@ function Topbar() {
         </div>
         
         <div className="topRight">
-          <img src={adminImage} alt="" className="topAvatar" />
+         {aimage ? ( 
+         <img src={aimage} alt="" className="topAvatar" />) : (
+        <img src={adminImage} alt="" className="topAvatar" />
+         )} 
         </div>
       </div>
     </div>
