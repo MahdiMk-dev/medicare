1d49871134f8ca256e270c074b8388675183c2fb
Mahdi Mokalled <mokaledmahdi@gmail.com>
2024-05-16 00:48:18 +0300

    duty model

--- user-side/backend/medicare-backend/app/Models/Duty.php
+++ user-side/backend/medicare-backend/app/Models/Duty.php
@@ -14,6 +14,13 @@ class Duty extends Model
         'request_id',
         // Add more fillable attributes as needed
     ];
-
+     public function order()
+    {
+        return $this->belongsTo(Order::class);
+    }
+     public function user()
+    {
+        return $this->belongsTo(User::class,'staff_id');
+    }
     // Define any relationships here
 }
