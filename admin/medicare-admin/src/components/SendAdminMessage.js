a9794125543c140a3c8b3747aae5f9b7dc29a8f1
Mahdi Mokalled <mokaledmahdi@gmail.com>
2024-05-15 12:40:47 +0300

    adjusted send admin message

--- admin/medicare-admin/src/components/SendAdminMessage.js
+++ admin/medicare-admin/src/components/SendAdminMessage.js
@@ -2,27 +2,39 @@ import React, { useState } from "react";
 import { db } from "../firebase";
 import { addDoc, collection, serverTimestamp } from "firebase/firestore";
 import { useParams } from "react-router-dom";
-
+import adminImage from '../images/icon-admin-32.png';
 const SendAdminMessage = () => {
   const { patientId } = useParams();
-
+  const image=localStorage.getItem('image_url');
    const sendMessage = async (event) => {
     event.preventDefault();
     if (message.trim() === "") {
       alert("Enter valid message");
       return;
     }
+    if (image) {
     //const { uid, displayName, photoURL } = auth.currentUser;
     await addDoc(collection(db, "messages"), {
       text: message,
       name: 'Admin',
-     // avatar: photoURL,
+      avatar: image,
       createdAt: serverTimestamp(),
       SenderId:1,
       ReceiverId:parseInt(patientId),
       read: false
 
-    });
+    })
+  }else{   await addDoc(collection(db, "messages"), {
+    text: message,
+    name: 'Admin',
+    avatar: adminImage,
+    createdAt: serverTimestamp(),
+    SenderId:1,
+    ReceiverId:parseInt(patientId),
+    read: false
+
+  });
+} 
     setMessage("");
   };
   const [message, setMessage] = useState("");
