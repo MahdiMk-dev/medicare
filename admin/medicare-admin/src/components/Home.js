efbe52f7ea39a4c209f394f260b28ed2febb59c4
Mahdi Mokalled <mokaledmahdi@gmail.com>
2024-05-16 00:45:03 +0300

    home component -admin

--- admin/medicare-admin/src/components/Home.js
+++ admin/medicare-admin/src/components/Home.js
@@ -2,17 +2,20 @@ import '../styles/home.css'
 
 import Topbar from './Topbar';
 import Sidebar from './Sidebar';
+import FeaturedInfo from './FeaturedInfo'
+import WidgetSm from './WidgetSm';
 
 function Home() {
+
   return (
     <div>
     <Topbar />
       <div className="admincontainer">
         <Sidebar />
     <div className="home">
-
+       <FeaturedInfo />
       <div className="homeWidgets">
-        
+        <WidgetSm/>
       </div>
     </div>
     </div>
