a70b332235e735396286dc0800fc29c7ed1a5524
Mahdi Mokalled <mokaledmahdi@gmail.com>
2024-05-16 00:54:01 +0300

    back up for user model

---
+++ user-side/backend/medicare-backend/app/Models/User2.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
+
+class user extends Authenticatable implements JWTSubject
+{
+    use HasFactory, Notifiable;
+
+    // Other class properties and methods...
+
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
+    
+    public function medications()
+    {
+        return $this->hasMany(Medication::class);
+    }
+
+    public function requests()
+    {
+        return $this->hasMany(PatientRequests::class);
+    }
+}
+
+
+
